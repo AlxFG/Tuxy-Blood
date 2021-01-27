92edcdb51dc968b4ca057e85d0766540ea30a16b
AlxFG <70714914+AlxFG@users.noreply.github.com>
2021-01-27 20:15:34 +0000

    Add files via upload
    Installer for arch based Linux distros

---
+++ arch.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+sudo pacman-Syu
+sudo pacman -S unrar
+sudo pacman -S wine-staging winetricks wine-mono 
+rm -rf ~/.wine
+WINEARCH=win32 WINEPREFIX=~/.wine wine wineboot
+wget https://github.com/doitsujin/dxvk/releases/download/v1.7.3/dxvk-1.7.3.tar.gz
+tar -xf dxvk-1.7.3.tar.gz
+cd dxvk-1.7.3
+export WINEPREFIX=~/.wine
+./setup_dxvk.sh install
+cd ~/Desktop
+wget https://download2266.mediafire.com/fvexxg6k22ng/n3p1v3jj9mcvnrf/MBAACC+-+Community+Edition.0.22.rar
+unrar x MBAACC+-+Community+Edition.0.22.rarMBAACC+-+Community+Edition.0.22.rar
+cd MBAACC - Community Edition/MBAACC
+rm d3d9_36.dll D3DX9_43.dll
